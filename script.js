e3d4d4d882177e7163569b5f729376c55a68143a
Aadarsh Mourya <en21cs301028@medicaps.ac.in>
2023-10-31 23:14:21 +0530

    Added a feature of Disabling the Start button after one Click in JS file

--- script.js
+++ script.js
@@ -3,20 +3,24 @@ let sec = 00;
 let minute = 00;
 let timer;
 let Num_Of_Laps = 0;
-var lapDiv = document.querySelector("#mainContent");           //for showing laps
+var lapDiv = document.querySelector("#mainContent"); // for showing laps
 
+let startButton = document.getElementById('start');
+let resetButton = document.getElementById('reset');
 
-document.getElementById('start').addEventListener('click', ()=>{
+startButton.addEventListener('click', () => {
     timer = true;
-    start();  
+    start();
+    startButton.disabled = true; // Disable start button on click
+    resetButton.disabled = false; // Enable reset button on click
 });
 
-
-document.getElementById('pause').addEventListener('click', ()=>{
+document.getElementById('pause').addEventListener('click', () => {
     timer = false;
-})
+    startButton.disabled = false; // Enable start button on pause
+});
 
-document.getElementById('reset').addEventListener("click", ()=>{
+resetButton.addEventListener("click", () => {
     timer = false;
     miliSec = 0;
     sec = 0;
@@ -24,19 +28,19 @@ document.getElementById('reset').addEventListener("click", ()=>{
     document.getElementById("minute").innerHTML = "00";
     document.getElementById("sec").innerHTML = "00";
     document.getElementById("miliSec").innerHTML = "00";
-})
+    startButton.disabled = false; // Enable start button on reset
+});
 
-
-function start(){
-    if(timer){
+function start() {
+    if (timer) {
         miliSec++;
-        if(miliSec == 100){
+        if (miliSec == 100) {
             sec++;
             miliSec = 0;
-            if(sec == 60){
+            if (sec == 60) {
                 minute++;
                 sec = 0;
-                miliSec=0;
+                miliSec = 0;
             }
         }
 
@@ -44,27 +48,25 @@ function start(){
         displaySec = sec;
         displayMili = miliSec;
 
-        if(minute < 10){
+        if (minute < 10) {
             displayMinute = "0" + minute;
         }
-        if(sec < 10){
+        if (sec < 10) {
             displaySec = "0" + sec;
         }
-        if(miliSec < 10){
+        if (miliSec < 10) {
             displayMili = "0" + miliSec;
         }
 
         document.getElementById("minute").innerHTML = displayMinute;
         document.getElementById("sec").innerHTML = displaySec;
         document.getElementById("miliSec").innerHTML = displayMili;
-        setTimeout(start,6.5); 
+        setTimeout(start, 6.5);
 
     }
 }
 
-
-document.getElementById("lap").addEventListener("click", (e)=>{
-
+document.getElementById("lap").addEventListener("click", (e) => {
     e.preventDefault();
     const CurrTime = displayMinute + " : " + displaySec + " : " + displayMili;
 
@@ -77,7 +79,6 @@ document.getElementById("lap").addEventListener("click", (e)=>{
 
     Lapsinfo.appendChild(Laps);
 
-
     //to create input and append it to Laps div                 //refer line 46 of HTML           
     LapNum = document.createElement("h2");
     LapNum.classList.add("lapNum");
@@ -89,20 +90,17 @@ document.getElementById("lap").addEventListener("click", (e)=>{
     LapVal.innerText = `${++Num_Of_Laps}`;
     Laps.appendChild(LapVal);
 
-
     //to create Laptime h2 and append it to Laps div            //refer line 47 of HTML
     LapTime = document.createElement("h2");
     LapTime.classList.add("lapTime");
     LapTime.innerHTML = CurrTime;
     Laps.appendChild(LapTime);
 
-
     //finally adding Lapsinfo div to lapDiv(main container)
-    
     lapDiv.appendChild(Lapsinfo);
 
-    document.getElementById("clear").addEventListener("click", ()=>{
+    document.getElementById("clear").addEventListener("click", () => {
         lapDiv.removeChild(Lapsinfo);
         Num_Of_Laps = 0;
-    })
-})
+    });
+});
